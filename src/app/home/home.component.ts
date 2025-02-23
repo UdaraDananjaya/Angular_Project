d89e036b87f266fb822333487bb56db1a1e5b0c1
Udara Dananjaya <udarapaymedia@gmail.com>
2025-02-23 10:02:18 +0530

    Add Home Changes

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,11 +1,23 @@
-import { Component } from '@angular/core';
-
+import { Component , OnInit } from '@angular/core';
+import { Course } from '../model/Course';
+import { CourseService } from '../Services/course.service';
+import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
-})
-export class HomeComponent {
+  // templateUrl: './home.component.html',  // Fixed: removed empty imports array
+  // styleUrls: ['./home.component.css']     // Fixed: changed styleUrl to styleUrls
 
+})
+export class HomeComponent implements OnInit {
+courses: Course[] = [];
+constructor(private courseService: CourseService) {
+  this.courses = this.courseService.getAll();
+}
+
+ngOnInit(): void {
+  this.courses = this.courseService.getAll();
+}
 }
