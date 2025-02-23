1ac0451d6f0cd995fd210bacb9cf078832b448d7
Udara Dananjaya <udarapaymedia@gmail.com>
2025-02-23 09:49:28 +0530

    Update Router Spelince Mistake

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,5 +2,5 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 
 export const routes: Routes = [
-    {path:'', compononent: HomeComponent},
+    {path:'', component: HomeComponent},
 ];
